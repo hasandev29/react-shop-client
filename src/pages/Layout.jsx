f22f078b1ba972f1ca86129b85d485840c19f912
Abul Hasan <hasandev29@gmail.com>
2023-08-10 12:21:19 +0530

    logo, name & responsive updated

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Announcement from "../components/Announcement"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import Newsletter from "../components/Newsletter"
+import { Outlet } from "react-router-dom";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
 
 export const Layout = () => {
   return (
     <>
-    <Announcement />
-    <Navbar />
-    <Outlet />
-    <Newsletter />
-    <Footer />
+      <Announcement />
+      <Navbar />
+      <Outlet />
+      <Newsletter />
+      <Footer />
     </>
-  )
-}
+  );
+};
